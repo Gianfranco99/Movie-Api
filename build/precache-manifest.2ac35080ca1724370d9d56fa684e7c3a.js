1e103411037204d13ccbb50f3488c8b2fc285302
Gianfranco99 <gianfranco.benvenuto99@gmail.com>
2021-02-04 10:51:40 -0300

    pages

---
+++ build/precache-manifest.2ac35080ca1724370d9d56fa684e7c3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70765957153bc7ec26cd3eb6983880f",
+    "url": "/Movie-Api/index.html"
+  },
+  {
+    "revision": "c1c04e920ab3b37fe971",
+    "url": "/Movie-Api/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "ac91fc3d214c464f25d1",
+    "url": "/Movie-Api/static/css/main.423d5b9f.chunk.css"
+  },
+  {
+    "revision": "c1c04e920ab3b37fe971",
+    "url": "/Movie-Api/static/js/2.01484b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-Api/static/js/2.01484b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac91fc3d214c464f25d1",
+    "url": "/Movie-Api/static/js/main.d0aea579.chunk.js"
+  },
+  {
+    "revision": "4050b7f8f812e8cd927e",
+    "url": "/Movie-Api/static/js/runtime-main.bc857a31.js"
+  }
+]);
